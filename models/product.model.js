a4d89e36f17d51e2647ede0461746eea127eb147
Mussorable <redduck5601@gmail.com>
2023-06-29 06:58:08 +0200

    add styles, details page

--- models/product.model.js
+++ models/product.model.js
@@ -1,4 +1,5 @@
 const setConnectDB = require("../database/connect.db").setConnectDB;
+const mongodb = require("mongodb");
 
 class Product {
   constructor(data) {
@@ -6,7 +7,6 @@ class Product {
     this.price = data.price;
     this.author = data.author;
     this["image-link"] = data["image-link"];
-    this.id = Math.floor(Math.random() * 10000);
   }
 
   async save() {
@@ -34,7 +34,20 @@ class Product {
     });
   }
 
-  // static getSingleProduct() {}
+  static getSingleProduct(id, callback) {
+    setConnectDB((db) => {
+      const books = db
+        .collection("books")
+        .find({ _id: new mongodb.ObjectId(id) })
+        .next()
+        .then((result) => {
+          callback(result);
+        })
+        .catch((error) => {
+          throw error;
+        });
+    });
+  }
 }
 
 module.exports = Product;
